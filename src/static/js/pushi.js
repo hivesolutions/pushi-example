2b02eeb29d9e5ff64c6b78141bc58b149ec92865
joamag <joamag@gmail.com>
2013-07-22 12:57:18 +0100

    new project files

---
+++ src/static/js/pushi.js
@@ -0,0 +1,120 @@
+var Pushi = function(appKey, options) {
+    var URL = "ws://localhost:9090/";
+    var self = this;
+
+    this.appKey = appKey;
+    this.options = options || {};
+    this.socket = new WebSocket(URL);
+    this.socketId = null;
+    this.state = "disconnected";
+    this.events = {};
+    this.authEndpoint = options.authEndpoint;
+
+    this.socket.onopen = function() {
+    };
+
+    this.socket.onmessage = function(event) {
+        var message = event.data;
+        var json = JSON.parse(message);
+
+        if (self.state == "disconnected"
+                && json.event == "pusher:connection_established") {
+            var data = JSON.parse(json.data);
+            self.socketId = data.socket_id;
+            self.state = "connected";
+            self.onoconnect();
+        } else if (self.state == "connected") {
+            self.onmessage(json);
+        }
+    };
+
+    this.socket.onclose = function() {
+        self.socketId = null;
+        self.state == "disconnected";
+        self.onodisconnect();
+    };
+};
+
+Pushi.prototype.trigger = function(event) {
+    var methods = this.events[event] || [];
+    for (var index = 0; index < methods.length; index++) {
+        var method = methods[index];
+        method.apply(this, arguments);
+    }
+};
+
+Pushi.prototype.bind = function(event, method) {
+    var methods = this.events[event] || [];
+    methods.push(method);
+    this.events[event] = methods;
+};
+
+Pushi.prototype.unbind = function(event, method) {
+    var methods = this.events[event] || [];
+    var index = methods.indexOf(method);
+    index && methods.splice(index, 1);
+};
+
+Pushi.prototype.onoconnect = function() {
+    this.subscribe("global");
+};
+
+Pushi.prototype.onodisconnect = function() {
+};
+
+Pushi.prototype.onsubscribe = function(channel) {
+};
+
+Pushi.prototype.onmessage = function(json) {
+    switch (json.event) {
+        case "pusher_internal:subscription_succeeded" :
+            this.onsubscribe(json.channel);
+            break;
+    }
+
+    this.trigger(json.event, json.data);
+};
+
+Pushi.prototype.send = function(json) {
+    var data = JSON.stringify(json);
+    this.socket.send(data);
+};
+
+Pushi.prototype.sendEvent = function(event, data) {
+    var json = {
+        event : event,
+        data : data
+    };
+    this.send(json);
+};
+
+if (typeof String.prototype.startsWith != "function") {
+    String.prototype.startsWith = function(string) {
+        return this.slice(0, string.length) == string;
+    };
+}
+
+Pushi.prototype.subscribe = function(channel) {
+    var isPrivate = channel.startsWith("private-");
+    if (isPrivate) {
+        return this.subscribePrivate(channel);
+    }
+
+    this.sendEvent("pusher:subscribe", {
+                channel : channel
+            });
+};
+
+Pushi.prototype.subscribePrivate = function(channel) {
+	if(!this.authEndpoint) {
+		throw "No auth endpoint defined";
+	}
+	
+	var request = new XMLHttpRequest();
+	request.open("GET", "demo_get.asp", true);
+	request.send();
+	
+	this.authEndpoint
+	
+	
+};
--- src/templates/index.html.tpl
+++ src/templates/index.html.tpl
@@ -2,8 +2,8 @@
 <html>
     <head>
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-        <script type="text/javascript" src="pushi.js"></script>
-        <script type="text/javascript" src="main.js"></script>
+        <script type="text/javascript" src="{{ url_for('static', filename = 'js/pushi.js') }}"></script>
+        <script type="text/javascript" src="{{ url_for('static', filename = 'js/main.js') }}"></script>
     </head>
     <body>
         This is just a simple test for the pushi infra-structure.
